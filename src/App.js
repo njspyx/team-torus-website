0a91b9255d95c033c89d1f23d8789ae2d0cb8c36
njspyx <neeljay03@gmail.com>
2023-11-14 17:21:51 -0500

    Basic coming soon layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Canvas, useFrame } from "@react-three/fiber";
+
+function Torus() {
+  const torusRef = React.useRef();
+
+  useFrame(() => {
+    if (torusRef.current) {
+      torusRef.current.rotation.y += 0.02;
+      torusRef.current.rotation.x += 0.01;
+    }
+  });
+
+  return (
+    <mesh ref={torusRef}>
+      <torusGeometry args={[5, 2, 16, 100]} />
+      <meshBasicMaterial color={0x0000ff} />
+    </mesh>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        <div className="header-text">
+          <h1 className="team-torus">Team Torus</h1>
+          <p className="coming-soon">Coming Soon</p>
+        </div>
       </header>
+      <div className="torus-container">
+        <Canvas>
+          <ambientLight />
+          <Torus />
+        </Canvas>
+      </div>
     </div>
   );
 }
